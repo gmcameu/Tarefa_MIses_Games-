776f855a7e720af7d7912c376e032dd7d20f1378
Bruno Marques <brunosmarques1995@hotmail.com>
2021-05-13 14:17:01 -0300

    Nova Tarefa

--- Soma_MG/Soma_MG/Origem.cpp
+++ Soma_MG/Soma_MG/Origem.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void Adiciona3(unsigned arg1, unsigned arg2)
+{
+	arg1 += 3;
+	arg2 += 3;
+}
+
 int main()
 {
-	int num_1;
-	int num_2;
-	int soma_1;
-	int soma_2;
+	unsigned num_1;
+	unsigned num_2;
 
 	cout << "Insira dois numeros inteiros: " << endl;
 
 	cin >> num_1 >> num_2;
 
-	soma_1 = num_1 + 3;
-	soma_2 = num_2 + 3;
+	Adiciona3(num_1, num_2);
 
-	cout << "Os novos numeros sao: " << soma_1 << " e " << soma_2;
+	cout << "Os novos numeros sao: " << num_1 << " e " << num_2;
+
+	return 0;
 }
